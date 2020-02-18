662716f4746cc0130c6cd9011ba55acfb47dbb0c
Jonathan Marbutt <jonathan@waycoolsw.com>
2020-02-18 09:59:52 -0600

    Adding Order Controller

---
+++ zapier-bham-dotnet/Controllers/OrdersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using zapier_bham_dotnet.Data;
+
+namespace zapier_bham_dotnet.Controllers
+{
+    [Route("api/orders")]
+    public class OrdersController : Controller
+    {
+        public OrdersDbContext DbContext { get; }
+
+        public OrdersController(OrdersDbContext dbContext)
+        {
+            DbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IQueryable<Order> GetOrders()
+        {
+            return DbContext.Orders;
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrder(Order order)
+        {
+            order.OrderDate = DateTime.Now;
+            DbContext.Orders.Add(order);
+
+            DbContext.SaveChanges();
+
+            return Ok();
+        }
+
+    }
+}
